65172ec359625e7dcfcb01950360add895a1f851
Rich Dammkoehler <rpd@noradltd.com>
2012-12-05 17:50:41 -0600

    Rich D: named the output file, updated .gitignore

--- makeit.sh
+++ makeit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clear; g++ -l gtest_main -l gtest -l pthread SodaMachineTest.cpp SodaMachine.cpp
+clear; g++ -l gtest_main -l gtest -l pthread SodaMachineTest.cpp SodaMachine.cpp -o test_sodamachine
